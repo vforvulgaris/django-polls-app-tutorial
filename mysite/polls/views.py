8a3585f353b5f3677f25fa6b46ab9a689f9a9602
_V <V@v_v_v.vv>
2020-05-21 17:15:19 +0300

    first simple template

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,9 +1,33 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 import datetime
 
 
 def index(request):
-    time_now = datetime.datetime.now()
-    return HttpResponse("Hello, world. You're at the polls index. {}".format(time_now))
+    #time_now = datetime.datetime.now()
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list, }
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+def aha(request):         #A-HA! view   /polls/aha/
+    return HttpResponse("You're at secret A-HA! page")
 
 # Create your views here.
